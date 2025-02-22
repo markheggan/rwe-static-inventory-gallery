677d9a5a93702365b4fe463b78097d1d414c1943
Mark Heggan <desk@markheggan.co.uk>
2025-02-22 06:06:48 +0000

    Navigation styles…

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,15 @@ const init = async () => {
     const year = new Date().getFullYear();
     copyrightYear.textContent = year;
   }
+  // Mobile Nav
+  const navBtn = document.getElementById("navBtn");
+  const navMenu = document.getElementById("navMenu");
+  if (navBtn && navMenu) {
+    navMenu.classList.toggle("is_collapsed");
+    navBtn.addEventListener("click", (event) => {
+      navMenu.classList.toggle("is_collapsed");
+    });
+  }
 };
 
 // Initialise on Document Ready
